d804f50a85cec3a380a334bd47eff120108ae269
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 18:57:44 +0900

    change Diff Modelの実装を行なった

--- app/Models/Diff.php
+++ app/Models/Diff.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Diff extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['source_text', 'compared_text'];
+
+    public function updatingUser(){
+        return $this->belongsTo(User::class, 'updating_user_id', 'id');
+    }
+
+    public function members(){
+        return $this->belongsToMany(User::class, 'members', 'diff_id', 'user_id');
+    }
 }
