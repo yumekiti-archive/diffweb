c4c8757a983a28b2ec81d0572625eaf26de6889f
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 19:05:21 +0900

    add MemberのRelationのメソッドuser diffを作成した

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Diff;
 
 class Member extends Model
 {
     use HasFactory;
+
+    /**
+     * dbのカラム user_id references id on users;
+     */
+    public function user()
+    {
+
+    }
+
+    /**
+     * dbのカラム diff_id references id on diffs;
+     */
+    public function diff()
+    {
+        
+    }
 }
