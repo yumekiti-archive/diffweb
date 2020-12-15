e2097dfb02f85a41bb2148991865a4742866e255
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 23:37:33 +0900

    migrationを実装した

--- database/migrations/2020_12_15_094122_create_user_invitations_table.php
+++ database/migrations/2020_12_15_094122_create_user_invitations_table.php
@@ -16,6 +16,18 @@ class CreateUserInvitationsTable extends Migration
         Schema::create('user_invitations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->bigInteger('invited_user_id')->index();
+            $table->bigInteger('invited_partner_id')->index();
+            $table->bigInteger('diff_id')->index();
+
+            $table->foreign('invited_user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onDelete('cascade');
+            $table->foreign('invited_partner_id')->references('id')->on('users');
+            $table->foreign('diff_id')->references('diff_id')->on('diffs')
+                ->onDelete('cascade')->onDelete('cascade');
+
+            // 同じDiffに対して同じユーザーが同じユーザーを複数回招待できないようにする。
+            $table->unique('invited_user_id', 'invited_partner_id', 'diff_id');
         });
     }
 
