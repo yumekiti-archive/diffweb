5dd13dce91ae283f265905fc6271b4300d5e04c0
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 23:47:04 +0900

    change onDelete onUpdateの制約を追加した

--- app/Models/UserInvitation.php
+++ app/Models/UserInvitation.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class UserInvitation extends Model
 {
     use HasFactory;
+
+    /**
+     * 招待した相手
+     */
+    public function invitedOpponentUser()
+    {
+
+    }
+
+    /**
+     * 招待したユーザー
+     */
+    public function invitedUser()
+    {
+
+    }
+
+    /**
+     * 招待先のDiffを取得する
+     */
+    public function diff(){
+        
+    }
 }
