11718d2e62a73e47e95fb7e359cac0a4e64a9184
kinoshita0623 <kuutarousii@gmail.com>
2020-12-17 00:46:54 +0900

    add routeを作成した。

---
+++ app/Http/Middleware/CheckDiffMember.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Diff;
+
+class CheckDiffMember
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check()){
+
+            $user = Auth::user();
+            $diff = Diff::findOrFail($request->route()->parameter('diffId'));
+            if( ! $diff->members()->find($user->id)){
+                // diffへアクセス権がない場合とりあえず401を返す
+                \abort(401);
+            }
+
+        }
+        
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiffController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +24,70 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
+
+
+Route::group(['middlware' => ['auth:sanctum']], function () {
+    
+    // アクセス可能なDiffを一覧表示します
+    Route::get('/diffs', [DiffController::class, 'all'])->name('diffs.all');
+
+    // Diffを新規作成します。
+    Route::post('/diffs', [DiffController::class, 'create']);
+
+
+    Route::group(['middleware' => ['member']], function () {
+        // アクセス可能なDiffをDiffのIdで取得しDiffを画面に表示します
+        // 現在編集モードで編集しているユーザーがいたらそれを表示してください。
+        // 他のユーザーが編集している時は編集できないようにしてください。
+        Route::get('/diffs/{diffId}', [DiffController::class, 'show'])->name('diffs.show');
+
+        // Diffを更新します
+        // 他のユーザーが編集しているにも関わらずputしようとしている場合はエラーを返してください。
+        Route::put('/diffs/{diffId}', [DiffController::class, 'update']);
+
+        // Diffを編集モードにします。
+        // 先客がいればエラーを返してください。
+        Rouge::post('/diffs/{diffId}/lock', [DiffController::class, 'lock']);
+
+        // Diffの編集モードを解除します。
+        // 編集モードを解除しているユーザーと編集中のユーザーが一致しているかチェックしてから解除してください。
+        Route::delete('/diffs/{diffId}/lock', [DiffController::class, 'unlock']);
+
+        // Diffにアクセスすることのできるメンバーを一覧表示します。
+        Route::get('/diffs/{diffId}/users', [MemberController::class, 'all'])->name('diffs.members.all');
+
+        // 一定期間内でパスワードを確認済みであればメンバーを削除します
+        // リクエストパラメーターにパスワードを含めてそのパスワードを検証します。
+        Route::delete('/diffs/{diffId}/users/{userId}', [MemberController::class, 'destroy'])->name('diffs.members.destroy');
+
+        // diffへの招待を一覧表示します。
+        Route::get('/diffs/{diffId}/invitations', [DiffController::class, 'invitations'])->name('diffs.invitations');
+
+        // ユーザーを招待します。
+        // user_idをpostパラメーターに含めるようにしてください。
+        Route::post('/diffs/{diffId}/invitations', [DiffController::class, 'create']);
+
+        // ユーザーへの招待を取り下げます。
+        Route::delete('/diffs/{diffId}/invitations/{invitationId}', [DiffController::class, 'cancelInvitation']);
+        
+
+
+    });
+
+    // 自分への招待を一覧表示します。
+    // またDiffのタイトルとDiffのIdを含めるようにしてください。
+    Route::get('/invitations', [InvitationController::class, 'invitations'])->name('invitations');
+
+    // 招待をacceptします。
+    Route::post('/invitations/{invitationId}/accept', [InvitationController::class, 'accept']);
+    
+    // 招待を拒否します。
+    Route::post('/invitations/{invitationId}/reject', [InvitationController::class, 'reject']);
+
+
+    
+});
+
+// user_nameでユーザーを検索します。
+Route::get('/users/{user_name?}', [UserController::class, 'search'])->name('users.search');
+
