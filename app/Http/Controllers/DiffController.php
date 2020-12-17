9d37b3f1c8dbad30c22cf157bab38ac7e5978833
kinoshita0623 <kuutarousii@gmail.com>
2020-12-18 01:28:09 +0900

    add 実装すべきメソッドを追加した

--- app/Http/Controllers/DiffController.php
+++ app/Http/Controllers/DiffController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Diff;
 
 class DiffController extends Controller
 {
-    //
+    
+    /**
+     * Pages/Diff/Index.vueを返すようにしてください。
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Pages/Diff/Edit.vueを返すようにしてください。
+     */
+    public function edit($diffId)
+    {
+
+    }
+
 }
