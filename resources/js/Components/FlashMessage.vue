61609b87d28cc4403e37aa4450bc5c9fd92e1275
kinoshita0623 <kuutarousii@gmail.com>
2020-12-19 23:37:46 +0900

    change fontawesome-free をインストールした

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!--成功したことを表すメッセージsuccessフラッシュを表示します。-->
+        <div v-if="$page.flash.success && isShow">
+
+        </div>
+
+        <!--失敗したことを表すerrorフラッシュを表示します。-->
+        <div>
+
+        </div>
+    </div>
+</template>
+<script>
+/**
+フラッシュメッセージの表示を担当します。
+ */
+export default {
+    data(){
+        return {
+            isShow: true
+        }
+    },
+    watch: {
+        '$page.flash': {
+            handler(){
+                this.isShow = true;
+            },
+            deep: true
+        }
+    }
+}
+</script>
