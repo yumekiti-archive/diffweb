359f0b921428e09f77dde18a51ae8e46f6da7c4e
kinoshita0623 <kuutarousii@gmail.com>
2020-12-22 14:43:05 +0900

    change 削除確認用のフォームを作った

--- resources/js/Pages/Diff/Members.vue
+++ resources/js/Pages/Diff/Members.vue
@@ -10,7 +10,7 @@
                     
                     <div>{{ member.user_name }}</div>
                     <div>
-                        <button @click="deleteMember(member.id)">
+                        <button @click="confirmDeleteMember(member)">
                             <i class="fas fa-user-times"></i>
                             除名
                         </button>
@@ -25,33 +25,88 @@
                 </div>
                 
             </div>
+            <jet-dialog-modal :show="confirmMemberDelection.isShow && confirmMemberDelection.member" @close="confirmMemberDelection.isShow = false">
+                <template #title>
+                    メンバー<span v-if="confirmMemberDelection.member">{{ confirmMemberDelection.member.user_name }}</span>を除名します。
+                </template>
+                <template #content>
+                    本当にメンバー{{ confirmMemberDelection.member.user_name }}を除名しますか？。
+                    除名する場合はパスワードを入力してください。
+                    <div>
+                        <jet-input type="password" class="mt-1 block w-3/4" placeholder="パスワード"
+                            ref="password" v-model="confirmMemberDelection.password" />
+                    </div>
+                </template>
+                <template #footer>
+                    <jet-secondary-button @click.native="cancelMemberDelection">キャンセル</jet-secondary-button>
+                    <jet-danger-button class="ml-2" @click.native="deleteMember(confirmMemberDelection.member)">
+                        除名
+                    </jet-danger-button>
+                </template>
+            </jet-dialog-modal>
         </d-card-content>
+
     </app-layout>
 </template>
 <script>
 import AppLayout from '../../Layouts/AppLayout';
 import CardContent from '../../Templetes/CardContent';
 import DiffNav from './DiffNav';
+import JetDialogModal from '@/Jetstream/DialogModal';
+import JetDangerButton from '@/Jetstream/DangerButton';
+import JetSecondaryButton from '@/Jetstream/SecondaryButton';
+import JetInput from '@/Jetstream/Input';
 export default {
     props:{
         users: Object,
         diff: Object
     },
+    data(){
+        return {
+            confirmMemberDelection: {
+                isShow: false,
+                member: null
+
+            },
+            form: {
+                password: ''
+            }
+        }
+    },
     components: {
         'app-layout': AppLayout,
         'd-card-content': CardContent,
         'd-diff-nav': DiffNav,
+        JetDialogModal,
+        JetDangerButton,
+        JetSecondaryButton,
+        JetInput
+
     },
     methods: {
         deleteMember(userId){
-            this.$inertia.delete(route('diffs.members.destroy', {
+            this.$inertia.visit(route('diffs.members.destroy', {
                 'diffId': this.diff.id,
                 'userId': userId
-            }), {
+            }), 
+            'delete',
+            {
+                    password: this.confirmMemberDelection.password,
+            },
+            {   
                 onFinish(){
-
+                    this.confirmedPassword = false;
+                    this.deleteMemberId = null;
                 }
             });
+        },
+        confirmDeleteMember(user){
+            this.confirmMemberDelection.isShow = true;
+            this.confirmMemberDelection.member = user;
+        },
+
+        cancelMemberDelection(){
+            this.confirmMemberDelection.isShow =false;
         }
     }
 }
