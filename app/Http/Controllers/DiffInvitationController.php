5bdb3454dd065aa326fcef9816f9f96938009d0f
kinoshita0623 <kuutarousii@gmail.com>
2020-12-22 16:47:03 +0900

    add Controllerのメソッド以外を実装した

--- app/Http/Controllers/DiffInvitationController.php
+++ app/Http/Controllers/DiffInvitationController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class DiffInvitationController extends Controller
 {
-    //
+    
+    /**
+     * 発行した招待を一覧表示します
+     */
+    public function invitations($diffId)
+    {
+
+    }
+
+    /**
+     * 発行した招待を取り下げます
+     */
+    public function cancel($diffId, $invitationId)
+    {
+
+    }
+
 }
--- app/Http/Controllers/UserInvitationController.php
+++ app/Http/Controllers/UserInvitationController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class UserInvitationController extends Controller
 {
-    //
+    
+    /**
+     * 自分への招待を一覧表示します
+     */
+    public function invitations()
+    {
+
+    }
+
+    /**
+     * 招待を受け入れます
+     */
+    public function accept($invitationId)
+    {
+
+    }
+
+    /**
+     * 招待を拒否します。
+     */
+    public function reject($invitationId)
+    {
+
+    }
 }
