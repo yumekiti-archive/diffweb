f3e5bff425698cef40dd23d0680a32838f523f1b
kinoshita0623 <kuutarousii@gmail.com>
2020-12-24 03:04:13 +0900

    add migrationを実装

--- database/migrations/2020_12_23_180017_create_diff_locks_table.php
+++ database/migrations/2020_12_23_180017_create_diff_locks_table.php
@@ -16,6 +16,13 @@ class CreateDiffLocksTable extends Migration
         Schema::create('diff_locks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->bigInteger('diff_id')->unsigned()->index();
+            $table->bigInteger('member_id')->unsigned()->index();
+
+            $table->unique(['diff_id']);
+            $table->foreign('diff_id')->references('id')->on('diffs')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
