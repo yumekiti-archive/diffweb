627838d12cad456f27acbc26379eb43b62e52901
kinoshita0623 <kuutarousii@gmail.com>
2020-12-24 03:46:23 +0900

    change Modelとテストケースに変更分の実装をした

--- app/Models/DiffLock.php
+++ app/Models/DiffLock.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Diff;
+use App\Models\Member;
 
 class DiffLock extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['member_id', 'diff_id'];
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'member_id');
+    }
+
+    public function user()
+    {
+        return $this->hasOneThrough(
+            User::class,
+            Member::class,
+            'id',
+            'id',
+            'member_id',
+            'user_id'
+        );
+    }
+
+    public function diff()
+    {
+        return $this->belongsTo(Diff::class, 'diff_id');
+    }
 }
