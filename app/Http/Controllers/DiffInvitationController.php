09b1aba41b39a2f749e105cb9aa1a9d5a6937949
kinoshita0623 <kuutarousii@gmail.com>
2020-12-24 00:11:17 +0900

    change Diffの招待一覧のControllerを実装した

--- app/Http/Controllers/DiffInvitationController.php
+++ app/Http/Controllers/DiffInvitationController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Diff;
 use App\Models\UserInvitation;
 use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
 
 class DiffInvitationController extends Controller
 {
@@ -17,7 +18,12 @@ class DiffInvitationController extends Controller
      */
     public function invitations($diffId)
     {
-
+        $diff = Auth::user()->diffs()->findOrFail($diffId);
+        $invitations = $diff->invitations()->paginat();
+        return Inertia::render('Diff/Invitation/Index', [
+            'invitations' => $invitations,
+            'diff' => $diff
+        ]);
     }
 
     /**
