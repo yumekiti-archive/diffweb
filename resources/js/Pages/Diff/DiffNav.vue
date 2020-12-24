77d5dbc66acf9b2dcb13d96d14cbd8227d3c5519
kinoshita0623 <kuutarousii@gmail.com>
2020-12-24 15:23:11 +0900

    change UIを調節した

--- resources/js/Pages/Diff/DiffNav.vue
+++ resources/js/Pages/Diff/DiffNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex space-x-4 mb-4" v-if="diff">
+    <div class="flex space-x-4" v-if="diff">
         <jet-nav-link :href="route('diffs.show', diff.id)" :active="route().current('diffs.show')">Diff</jet-nav-link>
         <jet-nav-link :href="route('diffs.members', diff.id)" :active="route().current('diffs.members')">メンバー</jet-nav-link>
         <jet-nav-link :href="route('diffs.invitations', diff.id)" :active="route().current('diffs.invitations')">招待</jet-nav-link>
--- resources/js/Pages/Diff/Invitations.vue
+++ resources/js/Pages/Diff/Invitations.vue
@@ -7,22 +7,31 @@
         <card-content>
 
         </card-content>
+        <pagination class="mt-4" :links="invitations.links" />
     </app-layout>
 </template>
 <script>
 import AppLayout from '@/Layouts/AppLayout';
 import CardContent from './../../Templetes/CardContent';
 import DiffNav from './DiffNav';
+import ItemUser from '../../Components/ItemUser';
+import Pagination from '../../Components/Pagination';
 
 export default {
     components: {
         AppLayout,
         CardContent,
         DiffNav,
+        ItemUser,
+        Pagination
 
     },
     props: {
         diff: {
+            type: Object,
+            required: true
+        },
+        invitations: {
             type: Object,
             required: true
         }
