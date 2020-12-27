43ba383174d7e4a3cd55dbb6a8e9b0a5953b749d
kinoshita0623 <kuutarousii@gmail.com>
2020-12-27 23:13:57 +0900

    change テストは通らないが原因として重複している可能性がある

--- tests/Feature/SearchUserTest.php
+++ tests/Feature/SearchUserTest.php
@@ -51,4 +51,72 @@ class SearchUserTest extends TestCase
         
     }
 
+    public function test連続したクエリの配列の中身は同じであるか()
+    {
+
+
+
+        $notMembers = User::leftJoin('members', 'users.id', '=', 'members.id')->where('members.diff_id', '<>', $this->diff->id)->get();
+        $notAnyMembers = User::leftJoin('members', 'users.id', '=', 'members.id')->whereNull('members.id')->get();
+        $beforeJoinCount =  $notMembers->count() + $notAnyMembers->count();
+
+        $merged = $notMembers->merge($notAnyMembers);
+        $array1 = [];
+        foreach($merged as $item){
+            $array1[$item->id] = $item;
+        }
+        $diff = $this->diff;
+
+        $notAnyMembersOrNotMember = User::leftJoin('members', 'users.id', '=', 'members.id')->where(function($query) use ($diff){
+            $query->whereNull('members.id')
+            ->orWhere('members.diff_id', '<>', $diff->id);
+        })->get();
+
+        Assert::assertEquals($merged->count(), $notAnyMembersOrNotMember->count());
+        Assert::assertNotEmpty($array1);
+        foreach($notAnyMembersOrNotMember as $user){
+            unset($array1[$user->id]);
+        }
+
+        Assert::assertEmpty($array1);
+
+        
+
+
+    }
+
+    public function test連続したクエリは有効か()
+    {
+        $notMemberCount = User::leftJoin('members', 'users.id', '=', 'members.id')->where('members.diff_id', '<>', $this->diff->id)->count();
+        $notAnyMemberCount = User::leftJoin('members', 'users.id', '=', 'members.id')->whereNull('members.id')->count();
+
+        $diffMemberCount = $this->diff->members()->count();
+
+        Assert::assertEquals(User::count(), $notMemberCount + $notAnyMemberCount + $diffMemberCount);
+
+        $count = $notMemberCount + $notAnyMemberCount + $diffMemberCount;
+
+        $diff = $this->diff;
+        $notAnyMemberCount = User::leftJoin('members', 'users.id', '=', 'members.id')->where(function($query) use ($diff){
+            $query->whereNull('members.id')
+            ->orWhere('members.diff_id', '<>', $diff->id);
+        })->count();
+        $diffMemberCount = $this->diff->members()->count();
+        
+        Assert::assertEquals($count, $notAnyMemberCount + $diffMemberCount);
+    }
+
+    public function test未所属のユーザーを正常に取得できるか()
+    {
+        $diff = $this->diff;
+        $notAnyMemberCount = User::leftJoin('members', 'users.id', '=', 'members.id')->where(function($query) use ($diff){
+            $query->whereNull('members.id')
+            ->orWhere('members.diff_id', '<>', $diff->id);
+        })->count();
+        $diffMemberCount = $this->diff->members()->count();
+        
+        Assert::assertEquals(User::count(), $notAnyMemberCount + $diffMemberCount);
+
+    }
+
 }
