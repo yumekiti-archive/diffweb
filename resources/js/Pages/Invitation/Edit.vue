268204d0772f5e4c7e16097b68e0beae94c3719e
kinoshita0623 <kuutarousii@gmail.com>
2020-12-27 20:56:41 +0900

    change 招待編集作成用の画面を追加した

---
+++ resources/js/Pages/Invitation/Edit.vue
@@ -0,0 +1,31 @@
+<template>
+    <app-layout>
+        <template #header>
+            <inertia-link :to="route('diffs.show', diff.id)">{{ diff.title }}</inertia-link>
+            / 招待作成
+        </template>
+
+    </app-layout>
+</template>
+<script>
+import AppLayout from '../../Layouts/AppLayout';
+import CardContent from '../../Templetes/CardContent';
+
+export default {
+    components: {
+        AppLayout,
+        CardContent
+    },
+    props: {
+        diff: {
+            type: Object,
+            required: true,
+
+        },
+        users: {
+            type: Object,
+            required: true
+        }
+    }    
+}
+</script>
