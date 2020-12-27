9f3cabb05702e7c104e92af68a7f6e5d0da455bf
yumekiti <yumekiti120440@gmail.com>
2020-12-28 04:46:26 +0900

    change sh

--- docker.sh
+++
@@ -1,3 +0,0 @@
-export user=$(id -u):$(id -g) && \
-docker-compose up --build -d && \
-docker exec -it diffweb_php bash
--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-composer install && \
-cp .env.example .env && \
-php artisan key:generate && \
-php artisan cache:clear && \
-php artisan config:clear && \
-php artisan route:clear && \
-php artisan view:clear && \
-php artisan migrate
+export user=$(id -u):$(id -g) && \
+docker-compose up --build -d && \
+docker-compose exec php composer install && \
+docker-compose exec php cp .env.example .env && \
+docker-compose exec php php artisan cache:clear && \
+docker-compose exec php php artisan config:clear && \
+docker-compose exec php php artisan route:clear && \
+docker-compose exec php php artisan view:clear && \
+docker-compose exec php php artisan key:generate && \
+docker-compose exec php php artisan migrate
