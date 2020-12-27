eb5be7ac5a308e6027a558f667fb046fe27083cf
kinoshita0623 <kuutarousii@gmail.com>
2020-12-28 04:56:59 +0900

    add シンプルなダイアログを追加した

---
+++ resources/js/Components/SimpleDialog.vue
@@ -0,0 +1,21 @@
+<template>
+    <jet-dialog-modal :show="isShow" @close="$emit('close')">
+        <template #title>
+            {{ title }}
+        </template>
+        <template #content>
+            {{ text }}
+        </template>
+        <template #footer>
+            <button @click="$emit('close')">いいえ</button>
+            <button @click="$emit('ok')">はい</button>
+        </template>
+    </jet-dialog-modal>
+</template>
+<script>
+import JetDialogModal from '@/Jetstream/DialogModal';
+
+export default {
+    props: ['title', 'text', 'show']
+}
+</script>
