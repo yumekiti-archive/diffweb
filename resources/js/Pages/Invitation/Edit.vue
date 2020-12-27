88cc49b9b81737abe33e389b93ebfc360764698f
kinoshita0623 <kuutarousii@gmail.com>
2020-12-28 02:57:25 +0900

    check 仕様変更するそうです

--- resources/js/Pages/Invitation/Edit.vue
+++ resources/js/Pages/Invitation/Edit.vue
@@ -4,19 +4,31 @@
             <diff-nav :diff="diff"></diff-nav>
 
         </template>
-        {{ users}}
+        <card-content>
+            <item-user v-for="user in users.data" :key="user.id" :user="user">
+                <button>
+                    <i class="fas fa-user-plus" />
+                    招待
+                </button>
+            </item-user>
+        </card-content>
+        <pagination :links="users.links" class="mt-4" />
     </app-layout>
 </template>
 <script>
 import AppLayout from '../../Layouts/AppLayout';
 import CardContent from '../../Templetes/CardContent';
 import DiffNav from '../Diff/DiffNav';
+import Pagination from '../../Components/Pagination';
+import ItemUser from '../../Components/ItemUser';
 
 export default {
     components: {
         AppLayout,
         CardContent,
-        DiffNav
+        DiffNav,
+        Pagination,
+        ItemUser
     },
     props: {
         diff: {
@@ -27,6 +39,15 @@ export default {
         users: {
             type: Object,
             required: true
+        }
+    },
+    methods: {
+        confirmInvite(user){
+
+        },
+
+        invite(user){
+            
         }
     }    
 }
