4be36071e464de3e9e0df3463c6daacf2e8da446
kinoshita0623 <kuutarousii@gmail.com>
2020-12-28 04:19:24 +0900

    change 作成画面が不要になった"

--- resources/js/Pages/Diff/DiffNav.vue
+++ resources/js/Pages/Diff/DiffNav.vue
@@ -3,7 +3,6 @@
         <jet-nav-link :href="route('diffs.show', diff.id)" :active="route().current('diffs.show')">Diff</jet-nav-link>
         <jet-nav-link :href="route('diffs.members', diff.id)" :active="route().current('diffs.members')">メンバー</jet-nav-link>
         <jet-nav-link :href="route('diffs.invitations', diff.id)" :active="route().current('diffs.invitations')">招待</jet-nav-link>
-        <jet-nav-link :href="route('diffs.invitations.new', diff.id)" :active="route().current('diffs.invitations.new')">招待作成</jet-nav-link>                    
     </div>
 </template>
 <script>
