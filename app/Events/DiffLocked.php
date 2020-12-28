9340e9782269ce433a74b0d6f396846fa59dcb69
kinoshita0623 <kuutarousii@gmail.com>
2020-12-28 15:05:41 +0900

    change Eventを実装した

--- app/Events/DiffLocked.php
+++ app/Events/DiffLocked.php
@@ -9,19 +9,24 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\DiffLock;
 
-class DiffLocked
+class DiffLocked implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $diffLock;
+
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(DiffLock $lock)
     {
         //
+        $this->lock;
     }
 
     /**
@@ -31,6 +36,7 @@ class DiffLocked
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        $diffId = $this->lock->diff()->first();
+        return new PrivateChannel('diffs.locked.' . $diffId);
     }
 }
