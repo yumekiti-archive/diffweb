efbd23a8cf7b5d5a33c3dcfe51750a3750ddae72
kinoshita0623 <kuutarousii@gmail.com>
2020-12-31 22:36:07 +0900

    change 招待を一覧表示できるようにした

--- resources/js/Pages/Invitation/Index.vue
+++ resources/js/Pages/Invitation/Index.vue
@@ -1,17 +1,67 @@
 <template>
+<app-layout>
+    <template #header>
+        招待一覧
+    </template>
     <card-content>
-        
+        <div 
+            v-for="invitation in invitations.data" :key="invitation.id"
+            class=" border-b py-2 pl-4 pr-4"
+        >
+            <div>
+                {{ invitation.author.user_name }}
+                からの招待
+            </div>
+
+            <div 
+                class="flex items-center justify-between"
+            >
+                <div>
+                    {{ invitation.diff.title }}
+
+                </div>
+                
+                <div>
+                    <button @click="reject(invitation)" class="mr-2">
+                        辞退
+                    </button>
+                    <button @click="accept(invitation)">
+                        承諾
+                    </button>
+                </div>
+            </div>
+        </div>
+
     </card-content>
+    <pagination-links :links="invitations.links" />
+</app-layout>
 </template>
 <script>
 import AppLayout from '@/Layouts/AppLayout';
 import CardContent from './../../Templetes/CardContent';
+import PaginationLinks from './../../Components/Pagination';
 
 
 export default {
     layout: AppLayout,
     components: {
-        CardContent
+        CardContent,
+        PaginationLinks
+    },
+    props: {
+        invitations: {
+            type: Object,
+            required: true
+        },
+        
+    },
+    methods: {
+        accept(invitation){
+            this.$inertia.post(route('invitations.accept', invitation.id));
+        },
+        reject(invitation){
+            this.$inertia.post(route('invitations.reject', invitation.id));
+        }
     }
 }
 </script>
